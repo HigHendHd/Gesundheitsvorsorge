c6c3ff74bdf2ff4cd3ff51ddd2eba6306dc3994d
Dennis <dennis.eigner55@gmail.com>
2015-11-11 13:18:42 +0100

    Hinzufügen des Navigationdrawers & Entfernen vom FAB

--- app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/MainActivity.java
+++ app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/MainActivity.java
@@ -1,13 +1,12 @@
 package gvapp.diplomprojekt.at.gv_appandroid;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.mikepenz.materialdrawer.DrawerBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +17,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        new DrawerBuilder().withActivity(this).build();
     }
 
     @Override
