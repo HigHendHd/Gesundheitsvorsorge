4c22782997f869de0ba306f9d110d06eb1df42b6
Dennis Eigner <dennis.eigner55@gmail.com>
2015-11-15 21:05:29 +0100

    KFA-Rechner erweitert

--- app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/Ernaehrung/KFA_Rechner/KfaFragment.java
+++ app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/Ernaehrung/KFA_Rechner/KfaFragment.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import gvapp.diplomprojekt.at.gv_appandroid.R;
 
@@ -14,6 +17,10 @@ import gvapp.diplomprojekt.at.gv_appandroid.R;
  */
 public class KfaFragment extends Fragment {
 
+    TextView tvFalte1, tvFalte2, tvFalte3, tvAlter, tvKfa, tvFalte1Id, tvFalte2Id, tvFalte3Id;
+    SeekBar sbFalte1, sbFalte2, sbFalte3, sbAlter;
+    RadioButton rbMaennlich, rbWeiblich;
+    boolean weiblich;
 
     public KfaFragment() {
         // Required empty public constructor
@@ -23,9 +30,79 @@ public class KfaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kfa, container, false);
+        View v = inflater.inflate(R.layout.fragment_kfa, container, false);
+
+        weiblich = true;
+
+        tvFalte1 = (TextView) v.findViewById(R.id.tvFalte1);
+        tvFalte2 = (TextView) v.findViewById(R.id.tvFalte2);
+        tvFalte3 = (TextView) v.findViewById(R.id.tvFalte3);
+        tvAlter = (TextView) v.findViewById(R.id.tvAlter);
+        tvKfa = (TextView) v.findViewById(R.id.tvKfa);
+        tvFalte1Id = (TextView) v.findViewById(R.id.tvFalte1Identifier);
+        tvFalte2Id = (TextView) v.findViewById(R.id.tvFalte2Identifier);
+        tvFalte3Id = (TextView) v.findViewById(R.id.tvFalte3Identifier);
+
+        sbFalte1 = (SeekBar) v.findViewById(R.id.sbFalte1);
+        sbFalte2 = (SeekBar) v.findViewById(R.id.sbFalte2);
+        sbFalte3 = (SeekBar) v.findViewById(R.id.sbFalte3);
+        sbAlter = (SeekBar) v.findViewById(R.id.sbAlter);
+
+        rbMaennlich = (RadioButton) v.findViewById(R.id.rbMaennlich);
+        rbWeiblich = (RadioButton) v.findViewById(R.id.rbWeiblich);
+
+        SeekBar.OnSeekBarChangeListener sbListener = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvFalte1.setText(sbFalte1.getProgress() + "");
+                tvFalte2.setText(sbFalte2.getProgress() + "");
+                tvFalte3.setText(sbFalte3.getProgress() + "");
+                tvAlter.setText(sbAlter.getProgress() + "");
+                tvKfa.setText(berechneKfa(sbFalte1.getProgress(), sbFalte2.getProgress(), sbFalte3
+                        .getProgress(), sbAlter.getProgress()) + "");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        };
+
+        sbAlter.setOnSeekBarChangeListener(sbListener);
+        sbFalte3.setOnSeekBarChangeListener(sbListener);
+        sbFalte2.setOnSeekBarChangeListener(sbListener);
+        sbFalte1.setOnSeekBarChangeListener(sbListener);
+
+        View.OnClickListener rbListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == rbWeiblich) {
+                    weiblich = true;
+                    tvFalte1Id.setText(R.string.trizezps);
+                    tvFalte2Id.setText(R.string.bauchfalte);
+                    tvFalte3Id.setText(R.string.hueftfalte);
+                } else {
+                    weiblich = false;
+                    tvFalte1Id.setText(R.string.brustfalte);
+                    tvFalte2Id.setText(R.string.bauchfalte);
+                    tvFalte3Id.setText(R.string.oberschenkelfalte);
+                }
+            }
+        };
+
+        rbWeiblich.setOnClickListener(rbListener);
+        rbMaennlich.setOnClickListener(rbListener);
+
+        return v;
     }
 
+    private double berechneKfa(int falte1, int falte2, int falte3, int alter) {
 
+        return 0;
+    }
 }
