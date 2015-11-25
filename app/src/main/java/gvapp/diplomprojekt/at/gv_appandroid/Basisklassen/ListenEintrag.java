e3a3d42708874319bc8f16c0556eefc0e8726e35
Dennis <dennis.eigner55@gmail.com>
2015-11-25 14:15:12 +0100

    Erste Vorbereitungen für Rezepte.

--- app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/Basisklassen/ListenEintrag.java
+++ app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/Basisklassen/ListenEintrag.java
@@ -1,5 +1,6 @@
 package gvapp.diplomprojekt.at.gv_appandroid.Basisklassen;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,15 +9,23 @@ import java.util.List;
  */
 public class ListenEintrag {
     protected List<String> lisTitel, lisUntertitel;
+    protected List<URL> lisURL;
+    private int workingInt = 0;
 
     public ListenEintrag() {
         lisTitel = new ArrayList<>();
         lisUntertitel = new ArrayList<>();
+        lisURL = new ArrayList<>();
     }
 
     public void addItem(String titel, String untertitel) {
         lisTitel.add(titel);
         lisUntertitel.add(untertitel);
+        workingInt++;
+    }
+
+    public void addUrl(URL url) {
+        lisURL.add(url);
     }
 
     public String getTitel(int position) {
--- app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/Ernaehrung/Rezepte/RezepteListe.java
+++ app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/Ernaehrung/Rezepte/RezepteListe.java
@@ -1,12 +1,17 @@
 package gvapp.diplomprojekt.at.gv_appandroid.Ernaehrung.Rezepte;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import gvapp.diplomprojekt.at.gv_appandroid.Basisklassen.Liste;
 import gvapp.diplomprojekt.at.gv_appandroid.Basisklassen.ListenEintrag;
+import gvapp.diplomprojekt.at.gv_appandroid.R;
 
 /**
  * Created by Dennis on 14.11.2015.
@@ -26,6 +31,21 @@ public class RezepteListe extends Liste {
         le.addItem("Breaking News", "Sack Reis umgefallen");
         super.setmAdapter(new RezepteAdapter(le));
 
+        createData(v);
+
         return v;
+    }
+
+    private void createData(View v) {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Snackbar.make(v, getActivity().getString(R.string.keininternet),
+                    Snackbar.LENGTH_INDEFINITE);
+        } else {
+            Snackbar.make(v, getActivity().getString(R.string.keininternet),
+                    Snackbar.LENGTH_INDEFINITE);
+        }
     }
 }
