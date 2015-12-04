a35f4a670b6cf55b30325f4bd6fd4969b27492e1
HigHendHd <dennis.eigner55@gmail.com>
2015-12-04 13:58:34 +0100

    Einige Dinge bei den Rezepten geändert.

---
+++ app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/Daten/Constants.java
@@ -0,0 +1,8 @@
+package gvapp.diplomprojekt.at.gv_appandroid.Daten;
+
+/**
+ * Created by Dennis on 04.12.2015.
+ */
+public abstract class Constants {
+    public static final String URL_REZEPTE = "http://gvapp.eig14830.webspace.spengergasse.at/data/rezepte/rezepteliste.xml";
+}
