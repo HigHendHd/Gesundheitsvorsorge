f8059c208f67adff3fc942fc5ec4a139217fd1e5
Dennis <denni.chou@gmail.com>
2016-02-22 20:07:52 +0100

    TEST

--- app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/Ernaehrung/Rezepte/Datenbank/Schritte.java
+++ app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/Ernaehrung/Rezepte/Datenbank/Schritte.java
@@ -4,4 +4,48 @@ package gvapp.diplomprojekt.at.gv_appandroid.Ernaehrung.Rezepte.Datenbank;
  * Created by Dennis on 21.02.2016.
  */
 public class Schritte {
+
+    private long r_id;
+    private String name;
+    private int anzahl;
+    private String einheit;
+
+    public Schritte(long r_id, String name, int anzahl, String einheit) {
+        this.r_id = r_id;
+        this.name = name;
+        this.anzahl = anzahl;
+        this.einheit = einheit;
+    }
+
+    public long getR_id() {
+        return r_id;
+    }
+
+    public void setR_id(long r_id) {
+        this.r_id = r_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAnzahl() {
+        return anzahl;
+    }
+
+    public void setAnzahl(int anzahl) {
+        this.anzahl = anzahl;
+    }
+
+    public String getEinheit() {
+        return einheit;
+    }
+
+    public void setEinheit(String einheit) {
+        this.einheit = einheit;
+    }
 }
--- app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/Ernaehrung/Rezepte/Datenbank/Zutaten.java
+++ app/src/main/java/gvapp/diplomprojekt/at/gv_appandroid/Ernaehrung/Rezepte/Datenbank/Zutaten.java
@@ -4,4 +4,38 @@ package gvapp.diplomprojekt.at.gv_appandroid.Ernaehrung.Rezepte.Datenbank;
  * Created by Dennis on 21.02.2016.
  */
 public class Zutaten {
+
+    private long r_id;
+    private int nummer;
+    private String text;
+
+    public Zutaten(long r_id, int nummer, String text) {
+        this.r_id = r_id;
+        this.nummer = nummer;
+        this.text = text;
+    }
+
+    public long getR_id() {
+        return r_id;
+    }
+
+    public void setR_id(long r_id) {
+        this.r_id = r_id;
+    }
+
+    public int getNummer() {
+        return nummer;
+    }
+
+    public void setNummer(int nummer) {
+        this.nummer = nummer;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
